3ac330405b871113b4fb642ca2cfa28cdab532e6
ali salah <alwaasalah@gmail.com>
2026-02-23 21:51:11 +0200

    Add files via upload

---
+++ assets/js/supabase-config.js
@@ -0,0 +1,13 @@
+
+// Initialize Supabase Client
+const SUPABASE_URL = 'url';
+const SUPABASE_KEY = 'key';
+
+let supabaseClient;
+
+if (typeof supabase !== 'undefined') {
+    supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+    console.log('Supabase Initialized');
+} else {
+    console.error('Supabase SDK not loaded!');
+}
